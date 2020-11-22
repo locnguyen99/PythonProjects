ff3e76b7e6c8c0182103c10d2eeb0972ff2ea7be
locnguyen <iot.loc.nguyen@gmail.com>
2020-11-22 11:43:00 +0700

    Add

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print("Hello Python world! lan 2")
+"""a = float(input('A='))
+b = float(input('B='))
+c = float(input('C='))
+
+delta =b*b-4*a*c
+
+if delta ==0:
+	print('Nghiem kep:x=', str(-b/2/a))
+if delta<0:
+	print('phuong trinh vo Nghiem')
+if delta>0:
+	print('x1='+str((-b+detal**0.5)/2/a))
+	print('x2='+str((-b-detal**0.5)/2/a))
+"""
+name = input('what is your name ')
+print ('My name is '+ name)
